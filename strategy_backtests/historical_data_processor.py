2ae0a25f3ecbbe959ed378603e947a9bbfe88865
harry <gaohaoyuandd@gmail.com>
2021-03-11 16:19:30 -0500

    added FTX data processor

--- strategy_backtests/historical_data_processor.py
+++ strategy_backtests/historical_data_processor.py
@@ -1,7 +1,7 @@
 import json
 import websockets
 import asyncio
-from keys import BINANCE_API_KEY, BINANCE_API_SECRET_KEY
+from keys import BINANCE_API_KEY, BINANCE_API_SECRET_KEY, FTX_API_KEY, FTX_API_SECRET
 from binance.client import Client
 import csv
 import os
@@ -11,6 +11,11 @@ from datetime import datetime as dt
 import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
+from typing import Optional, Dict, Any, List 
+import urllib.parse  
+from requests import Request, Session, Response
+import hmac
+from ciso8601 import parse_datetime
 
 # Binance OHCL data
 class BinanceDataProcessor:
@@ -33,34 +38,183 @@ class BinanceDataProcessor:
         ohlc_data.close()
 
 
-# cctx ftx
-# import ccxt  # ccxt only run on python 3.8.5 but not 3.9.0
-# import pandas as pd
-# import datetime
-# import asyncio
-# import csv
+class FTXDataProcessor:
+    
+    _ENDPOINT = 'https://ftx.com/api/'
 
-# ftx = ccxt.ftx()
+    def __init__(self, api_key=None, api_secret=None, subaccount_name=None):
+        self._session = Session()
+        self._api_key = api_key
+        self._api_secret = api_secret
+        self._subaccount_name = subaccount_name
 
-# def fetch_data_thru_cctx(symbol='BTC-PERP', time_interval='1m', since='2021-02-25T01:29:00.000Z', limit=1500, params={}):
+    def _get(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('GET', path, params=params)
 
-#     since = ftx.parse8601(since)
+    def _post(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('POST', path, json=params)
 
-#     data = ftx.fetch_ohlcv(symbol, time_interval, since, limit)
-#     data_framed = pd.DataFrame(data)
+    def _delete(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('DELETE', path, json=params)
 
-#     data_framed.columns = (
-#         ['UTC timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    def _request(self, method: str, path: str, **kwargs) -> Any:
+        request = Request(method, self._ENDPOINT + path, **kwargs)
+        self._sign_request(request)
+        response = self._session.send(request.prepare())
+        return self._process_response(response)
 
-#     def parsing_UNIX(unix):  # parsing unix timestamp
-#         return ftx.iso8601(unix)
-#     data_framed['UTC timestamp'] = data_framed['UTC timestamp'].apply(
-#         parsing_UNIX)
-#     file = open('recent_kline_data.csv', 'w', newline='')
-#     candlestick_writer = csv.writer(file, delimiter=',')
-#     print(data_framed)
-#     # print(data_framed.iloc[:1])
-#     # print(data_framed.iloc[:1].iloc[:, 1])
+    def _sign_request(self, request: Request) -> None:
+        ts = int(time.time() * 1000)
+        prepared = request.prepare()
+        signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode(
+        )
+        if prepared.body:
+            signature_payload += prepared.body
+        signature = hmac.new(self._api_secret.encode(),
+                             signature_payload, 'sha256').hexdigest()
+        request.headers['FTX-KEY'] = self._api_key
+        request.headers['FTX-SIGN'] = signature
+        request.headers['FTX-TS'] = str(ts)
+        if self._subaccount_name:
+            request.headers['FTX-SUBACCOUNT'] = urllib.parse.quote(
+                self._subaccount_name)
+
+    def _process_response(self, response: Response) -> Any:
+        try:
+            data = response.json()
+        except ValueError:
+            response.raise_for_status()
+            raise
+        else:
+            if not data['success']:
+                raise Exception(data['error'])
+            return data['result']
+
+    def list_futures(self) -> List[dict]:
+        return self._get('futures')
+
+    def list_markets(self) -> List[dict]:
+        return self._get('markets')
+
+    def get_orderbook(self, market: str, depth: int = None) -> dict:
+        return self._get(f'markets/{market}/orderbook', {'depth': depth})
+
+    def get_trades(self, market: str) -> dict:
+        return self._get(f'markets/{market}/trades')
+
+    def get_account_info(self) -> dict:
+        return self._get(f'account')
+
+    def get_open_orders(self, market: str = None) -> List[dict]:
+        return self._get(f'orders', {'market': market})
+
+    def get_order_history(self, market: str = None, side: str = None, order_type: str = None, start_time: float = None, end_time: float = None) -> List[dict]:
+        return self._get(f'orders/history', {'market': market, 'side': side, 'orderType': order_type, 'start_time': start_time, 'end_time': end_time})
+
+    def get_conditional_order_history(self, market: str = None, side: str = None, type: str = None, order_type: str = None, start_time: float = None, end_time: float = None) -> List[dict]:
+        return self._get(f'conditional_orders/history', {'market': market, 'side': side, 'type': type, 'orderType': order_type, 'start_time': start_time, 'end_time': end_time})
+
+    def modify_order(
+        self, existing_order_id: Optional[str] = None,
+        existing_client_order_id: Optional[str] = None, price: Optional[float] = None,
+        size: Optional[float] = None, client_order_id: Optional[str] = None,
+    ) -> dict:
+        assert (existing_order_id is None) ^ (existing_client_order_id is None), \
+            'Must supply exactly one ID for the order to modify'
+        assert (price is None) or (
+            size is None), 'Must modify price or size of order'
+        path = f'orders/{existing_order_id}/modify' if existing_order_id is not None else \
+            f'orders/by_client_id/{existing_client_order_id}/modify'
+        return self._post(path, {
+            **({'size': size} if size is not None else {}),
+            **({'price': price} if price is not None else {}),
+            ** ({'clientId': client_order_id} if client_order_id is not None else {}),
+        })
+
+    def get_conditional_orders(self, market: str = None) -> List[dict]:
+        return self._get(f'conditional_orders', {'market': market})
+
+    def place_order(self, market: str, side: str, price: float, size: float, type: str = 'limit',
+                    reduce_only: bool = False, ioc: bool = False, post_only: bool = False,
+                    client_id: str = None) -> dict:
+        return self._post('orders', {'market': market,
+                                     'side': side,
+                                     'price': price,
+                                     'size': size,
+                                     'type': type,
+                                     'reduceOnly': reduce_only,
+                                     'ioc': ioc,
+                                     'postOnly': post_only,
+                                     'clientId': client_id,
+                                     })
+
+    def place_conditional_order(
+        self, market: str, side: str, size: float, type: str = 'stop',
+        limit_price: float = None, reduce_only: bool = False, cancel: bool = True,
+        trigger_price: float = None, trail_value: float = None
+    ) -> dict:
+        """
+        To send a Stop Market order, set type='stop' and supply a trigger_price
+        To send a Stop Limit order, also supply a limit_price
+        To send a Take Profit Market order, set type='trailing_stop' and supply a trigger_price
+        To send a Trailing Stop order, set type='trailing_stop' and supply a trail_value
+        """
+        assert type in ('stop', 'take_profit', 'trailing_stop')
+        assert type not in ('stop', 'take_profit') or trigger_price is not None, \
+            'Need trigger prices for stop losses and take profits'
+        assert type not in ('trailing_stop',) or (trigger_price is None and trail_value is not None), \
+            'Trailing stops need a trail value and cannot take a trigger price'
+
+        return self._post('conditional_orders',
+                          {'market': market, 'side': side, 'triggerPrice': trigger_price,
+                           'size': size, 'reduceOnly': reduce_only, 'type': 'stop',
+                           'cancelLimitOnTrigger': cancel, 'orderPrice': limit_price})
+
+    def cancel_order(self, order_id: str) -> dict:
+        return self._delete(f'orders/{order_id}')
+
+    def cancel_orders(self, market_name: str = None, conditional_orders: bool = False,
+                      limit_orders: bool = False) -> dict:
+        return self._delete(f'orders', {'market': market_name,
+                                        'conditionalOrdersOnly': conditional_orders,
+                                        'limitOrdersOnly': limit_orders,
+                                        })
+
+    def get_fills(self) -> List[dict]:
+        return self._get(f'fills')
+
+    def get_balances(self) -> List[dict]:
+        return self._get('wallet/balances')
+
+    def get_deposit_address(self, ticker: str) -> dict:
+        return self._get(f'wallet/deposit_address/{ticker}')
+
+    def get_positions(self, show_avg_price: bool = False) -> List[dict]:
+        return self._get('positions', {'showAvgPrice': show_avg_price})
+
+    def get_position(self, name: str, show_avg_price: bool = False) -> dict:
+        return next(filter(lambda x: x['future'] == name, self.get_positions(show_avg_price)), None)
+
+    def get_all_trades(self, market: str, start_time: float = None, end_time: float = None) -> List:
+        ids = set()
+        limit = 100
+        results = []
+        while True:
+            response = self._get(f'markets/{market}/trades', {
+                'end_time': end_time,
+                'start_time': start_time,
+            })
+            deduped_trades = [r for r in response if r['id'] not in ids]
+            results.extend(deduped_trades)
+            ids |= {r['id'] for r in deduped_trades}
+            print(f'Adding {len(response)} trades with end time {end_time}')
+            if len(response) == 0:
+                break
+            end_time = min(parse_datetime(t['time'])
+                           for t in response).timestamp()
+            if len(response) < limit:
+                break
+        return results
 
 
 # API Doc: https://docs.deribit.com/?python#public-get_instrument
@@ -179,8 +333,13 @@ class DeribitDataProcessor:
 
 if __name__ == '__main__':
 
-    deribit = DeribitDataProcessor('2021', '02', '26', time_interval='30')
+    # deribit = DeribitDataProcessor('2021', '02', '26', time_interval='30')
     # df = res.to_pandas_df(res.retrieve_data())
-    complete_data = deribit.REST_polling(True, 'BTCPerp-09-26-20-to-03-01-21')
+    # complete_data = deribit.REST_polling(True, 'BTCPerp-09-26-20-to-03-01-21')
     # dataframe = pd.read_csv('BTCPerp-09-26-20-to-03-01-21.csv')
     # plt.plot(dataframe.timestamp, dataframe.close)
+
+# market: str, start_time: float = None, end_time: float = None
+    acc = FTXDataProcessor(api_key=FTX_API_KEY, api_secret=FTX_API_SECRET)
+    res = acc.get_all_trades(market='BTC-PERP')
+    print(res)
