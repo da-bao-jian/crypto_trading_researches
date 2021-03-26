689f9c51eead35d29920576b3165355462e0a96b
harry <gaohaoyuandd@gmail.com>
2021-03-26 11:40:13 -0400

    added nubmers on heatmap

--- strategy_backtests/utility_classes/correlation.py
+++ strategy_backtests/utility_classes/correlation.py
@@ -4,13 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import traceback
+import numpy as np
 
 class Correlation:
 
     def __init__(self, spread_folder_path: str):
         self.spread_folder_path = spread_folder_path
     
-    def spreads_correlation_heatmap(self, futures_date: str, annot: bool=False):
+    def spreads_correlation_heatmap(self, futures_date: str, annot: bool=False, triangular: bool=False, min_cor: int = -1.0, max_cor: int=1.0):
 
         spread_df = pd.DataFrame() #making a dataframe for spreads across different tokens
         errors=[]
@@ -22,17 +23,17 @@ class Correlation:
                         spread_df[token_name] = pd.read_csv(fut_data.path)['spread_close']
 
             corr_matrix = spread_df.corr(method='pearson')
-            # f, ax = plt.subplots(figsize=(11, 9))
             cmap = sns.diverging_palette(220, 10, as_cmap=True)
 
+            if triangular:
             # Generate a mask for the upper triangle
-            #mask = np.zeros_like(corr_matrix, dtype=np.bool)
-            #mask[np.triu_indices_from(mask)] = True
+                mask = np.zeros_like(corr_matrix, dtype=np.bool)
+                mask[np.triu_indices_from(mask)] = True
+            else:
+                mask = None
 
-            
-
-            sns.heatmap(corr_matrix, cmap=cmap, vmax=1.0,
-                        vmin=-1.0, linewidths=0.5, annot=annot)
+            sns.heatmap(corr_matrix, cmap=cmap, vmax=max_cor,
+                        vmin=min_cor, linewidths=0.5, annot=annot, mask=mask)
             plt.title('{} spreads Pearson correlation'.format(futures_date))
             plt.show()
                 
@@ -42,6 +43,3 @@ class Correlation:
 
         for e in errors:
             print(e)
-
-
-        # return corr_matrix
